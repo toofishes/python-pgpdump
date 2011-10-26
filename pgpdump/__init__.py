8ef0152c3ad851c9a68d19edabeb9ec5f1b701a7
Dan McGee <dpmcgee@gmail.com>
2011-10-25 23:59:36 -0500

    Data wrapper cleanup and improvements
    Don't store offset as state on the BinaryData object; we don't need it
    outside of the generator function and there is no good reason to prevent
    someone from having multiple iterators.
    
    AsciiData now checks the CRC-24 value if it can find it against the
    processed and decoded PGP data.
    
    Signed-off-by: Dan McGee <dpmcgee@gmail.com>

--- pgpdump/__init__.py
+++ pgpdump/__init__.py
@@ -22,31 +22,34 @@ class BinaryData(object):
             raise Exception("incorrect binary data")
         self.data = data
         self.length = len(data)
-        self.offset = 0
 
     def packets(self):
-        while self.offset < len(self.data):
-            total_length, packet = construct_packet(self.data[self.offset:])
+        '''A generator function returning PGP data packets.'''
+        offset = 0
+        while offset < len(self.data):
+            total_length, packet = construct_packet(self.data[offset:])
             # increment our data pointer
-            self.offset += total_length
+            offset += total_length
             yield packet
 
-    def reset(self):
-        self.offset = 0
-
     def __repr__(self):
-        return "<%s: length %d, offset %d>" % (
-                self.__class__.__name__, self.length, self.offset)
+        return "<%s: length %d>" % (
+                self.__class__.__name__, self.length)
 
 class AsciiData(BinaryData):
     '''A wrapper class that supports ASCII-armored input. It searches for the
     first PGP magic header and extracts the data contained within.'''
-    def __init__(self, data, strip_magic=True):
+    def __init__(self, data):
         self.original_data = data
-        if strip_magic:
-            data = self.strip_magic(data)
-        data = b64decode(data)
-        data = [ord(c) for c in data]
+        data = self.strip_magic(data)
+        data, known_crc = self.split_data_crc(data)
+        data = [ord(c) for c in b64decode(data)]
+        if known_crc:
+            # verify it if we could find it
+            actual_crc = self.crc24(data)
+            if known_crc != actual_crc:
+                raise Exception("CRC failure: known 0x%x, actual 0x%x" % (
+                    known_crc, actual_crc))
         super(AsciiData, self).__init__(data)
 
     @staticmethod
@@ -54,7 +57,7 @@ class AsciiData(BinaryData):
         '''Strip away the '-----BEGIN PGP SIGNATURE-----' and related cruft so
         we can safely base64 decode the remainder.'''
         magic = '-----BEGIN PGP '
-        ignore = '-----BEGIN PGP SIGNED '
+        #ignore = '-----BEGIN PGP SIGNED '
 
         # find our magic string
         idx = data.find(magic)
@@ -75,6 +78,20 @@ class AsciiData(BinaryData):
         return data
 
     @staticmethod
+    def split_data_crc(data):
+        '''The Radix-64 format appends any CRC checksum to the end of the data
+        block, in the form '=alph', where there are always 4 ASCII characters
+        correspnding to 3 digits (24 bits). Look for this special case.'''
+        # don't let newlines trip us up
+        data = data.strip()
+        if data[-5] == '=':
+            # CRC is returned without the = and converted to a decimal
+            crc = b64decode(data[-4:])
+            crc = (ord(crc[0]) << 16) + (ord(crc[1]) << 8) + ord(crc[2])
+            return (data[:-5], crc)
+        return (data, None)
+
+    @staticmethod
     def crc24(data):
         '''Implementation of the OpenPGP CRC-24 algorithm.'''
         # CRC-24-Radix-64
@@ -85,7 +102,7 @@ class AsciiData(BinaryData):
         for byte in data:
             crc ^= (byte << 16)
             # optimization: don't have to call range(8) here
-            for i in (0, 1, 2, 3, 4, 5, 6, 7):
+            for _ in (0, 1, 2, 3, 4, 5, 6, 7):
                 crc <<= 1
                 if crc & 0x01000000:
                     crc ^= 0x00864cfb
