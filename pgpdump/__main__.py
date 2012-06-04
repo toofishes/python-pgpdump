4c0150dccb09adccaa3e671277ff6758301e2960
Tobias Mueller (ideabox) <tobiasmue@gnome.org>
2012-06-04 10:54:00 +0200

    Made the parsefile function yield packets it read
    This changes the interface of the function but I don't want to replicate
    that function in all the filters that I apply on top of the parsed data.
    My usecase is smth like this:
        for filename in sys.argv[1:]:
            for packet in ifilter(filter, parsefile(filename)):
                print(packet)
    
    Signed-off-by: Dan McGee <dpmcgee@gmail.com>

--- pgpdump/__main__.py
+++ pgpdump/__main__.py
@@ -9,16 +9,19 @@ def parsefile(name):
             data = AsciiData(infile.read())
         else:
             data = BinaryData(infile.read())
-    counter = length = 0
+
     for packet in data.packets():
-        counter += 1
-        length += packet.length
-    print('%d packets, length %d' % (counter, length))
+        yield packet
 
 
 def main():
+    counter = length = 0
     for filename in sys.argv[1:]:
-        parsefile(filename)
+        for packet in parsefile(filename):
+            counter += 1
+            length += packet.length
+
+    print('%d packets, length %d' % (counter, length))
 
 
 if __name__ == '__main__':
